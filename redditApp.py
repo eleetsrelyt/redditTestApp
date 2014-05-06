2532102006f68dc14fed326d6fd278f8b9377c8c
Tyler Steele <steeletyler@gmail.com>
2014-05-05 22:16:03 -0600

    search and return results from subreddits
    search and return the subreddit name from hot posts
    search and return posts based on karma from hot posts

--- redditApp.py
+++ redditApp.py
@@ -1,5 +1,14 @@
 import praw
-r = praw.Reddit(user_agent='test_app')
-submissions = r.get_subreddit('opensource').get_hot(limit=5)
-[str(x) for x in submissions]
+r = praw.Reddit(user_agent='app')
 
+def subsearch(sub='python', n=10):
+    submissions = r.get_subreddit(sub).get_hot(limit=n)
+    for x in submissions:
+        if str(x).find(sub) > 0 or str(x).find(sub.capitalize()) > 0 or str(x).find(sub.upper()) > 0:
+            print x
+
+def greaterthan(sub='python', n=10, k=5):
+    submissions = r.get_subreddit(sub).get_hot(limit=n)
+    for x in submissions:
+        if int(str(x)[0:str(x).find(' ')]) >= k:
+            print x
