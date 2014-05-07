94f2db8dc9ee9cfbd5a6600d7437e5ad9d7e7610
Tyler Steele <steeletyler@gmail.com>
2014-05-06 22:33:20 -0600

    update user_agent

--- redditApp.py
+++ redditApp.py
@@ -1,17 +1,24 @@
 import praw
-r = praw.Reddit(user_agent='app')
+import time
+user_agent = ("This is me playing with the Reddit API praw for Python. ")
+r = praw.Reddit(user_agent=user_agent)
 
-def subsearch(sub='python', n=10):
+def subReturnTitles(sub='python', n=10):
+    submissions = r.get_subreddit(sub).get_top(limit=n)
+    for x in submissions:
+        titleOnly(str(x))
+
+def subSearch(sub='python', n=10):
     submissions = r.get_subreddit(sub).get_top(limit=n)
     for x in submissions:
         if str(x).find(sub) > 0 or str(x).find(sub.capitalize()) > 0 or str(x).find(sub.upper()) > 0:
-            print x
+            titleOnly(str(x))
 
-def greaterthan(sub='python', n=10, k=5):
+def subGreaterThanK(sub='python', n=10, k=5):
     submissions = r.get_subreddit(sub).get_top(limit=n)
     for x in submissions:
         if int(str(x)[0:str(x).find(' ')]) >= k:
-            print x
+            titleOnly(str(x))
 
-def titleonly(i):
-    return i[i.find(' :: ')+4:]
+def titleOnly(i):
+    print i[i.find(' :: ')+4:]
